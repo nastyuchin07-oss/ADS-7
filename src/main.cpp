d33f854c0ecc90643b97cd1aaac9f84dc68293fa
nastyuchin07-oss <nastyuchin07@gmail.com>
2026-05-27 20:42:08 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,27 @@
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
 
-  while (count--)
-    train.addCar(false);
+    Train train;
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    std::cout << "Добавляем вагоны в поезд:" << std::endl;
+    train.AddCar(false);
+    train.AddCar(true);
+    train.AddCar(false);
+    train.AddCar(true);
+    train.AddCar(false);
+
+    std::cout << "Реальная длина поезда: " << train.GetRealLength() << std::endl;
+
+    int computed_length = train.GetLength();
+    std::cout << "Вычисленная длина: " << computed_length << std::endl;
+    std::cout << "Количество переходов: " << train.GetOpCount() << std::endl;
+
+    if (computed_length == train.GetRealLength()) {
+        std::cout << "\nАлгоритм работает корректно!" << std::endl;
+    } else {
+        std::cout << "\nОшибка: вычисленная длина не совпадает с реальной!" << std::endl;
+    }
+
+    return 0;
 }
